4e4159d8df99e2e3f72366180874318dc461d896
nusje2000 <maarten.nusteling@gmail.com>
2020-01-05 14:38:39 +0100

    Fixed support to internal dependencies

--- src/Validator/Rule/MissingDependencyRule.php
+++ src/Validator/Rule/MissingDependencyRule.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Nusje2000\ComposerMonolith\Validator\Rule;
 
-use Nusje2000\DependencyGraph\DependencyGraph;
 use Nusje2000\ComposerMonolith\Validator\RuleInterface;
 use Nusje2000\ComposerMonolith\Validator\Violation\MissingDependencyViolation;
 use Nusje2000\ComposerMonolith\Validator\ViolationCollection;
+use Nusje2000\DependencyGraph\DependencyGraph;
 
 final class MissingDependencyRule implements RuleInterface
 {
@@ -19,6 +19,11 @@ final class MissingDependencyRule implements RuleInterface
 
         foreach ($subPackages as $subPackage) {
             foreach ($subPackage->getDependencies() as $dependency) {
+                // skip references to internal packages
+                if ($subPackages->hasPackageByName($dependency->getName())) {
+                    continue;
+                }
+
                 if (!$rootPackage->hasDependency($dependency->getName())) {
                     $violations->append(new MissingDependencyViolation($subPackage, $dependency));
                 }
